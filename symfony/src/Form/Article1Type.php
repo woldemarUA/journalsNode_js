7076db16f2b48130d2452cdd41e84d23cacd0346
Vlad Tymchenko <vlad.timchenko@gmail.com>
2024-03-04 15:48:45 +0100

    Started to make entity upload

--- symfony/src/Form/Article1Type.php
+++ symfony/src/Form/Article1Type.php
@@ -8,13 +8,16 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class Article1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('author')
+            ->add('author', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
             ->add('title')
             ->add('description')
             ->add('image')
--- symfony/src/Form/Article2Type.php
+++ symfony/src/Form/Article2Type.php
@@ -8,19 +8,39 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class Article2Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('author')
-            ->add('title')
-            ->add('description')
-            ->add('image')
-            // ->add('isApproved')
-            // ->add('createdAt')
-            // ->add('updatedAt')
+            ->add('author', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('title', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('description', TextareaType::class, [
+                // Optional configuration
+                'attr' => ['class' => 'form-control'],
+                'required' => false,
+                'label' => 'journal description',
+            ])
+            ->add('image', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'Upload file',
+                // This is not needed for HTML5 compliant browsers
+                'attr' => [
+                    'accept' => 'image/*',
+                    'class' => 'form-control'
+                ],
+            ])
+
             ->add('userId', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'id',
